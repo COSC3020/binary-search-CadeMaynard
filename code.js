379b5dab91b8c73a714582fa7747b6d24c680526
CadeMaynard <143521268+CadeMaynard@users.noreply.github.com>
2023-09-19 11:33:54 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
 function binarySearch(list, element) {
-    return -1;
+    var mid = Math.floor(list.length/2), high = list.length, low = 0, index = -1;
+    
+    if(list.length <= 1) {
+        if(list[0] == element)
+            return 0;
+        else
+            return -1;
+    } else if(list[low] == element) {
+        return low
+    } else if(list[mid] > element) {
+        return low + binarySearch(list.slice(low, mid), element)
+    } else if(list[mid] == element) {
+        return mid
+    } else {
+        return low + binarySearch(list.slice(mid + 1, list.length), element)
+    }
 }
+
+/*
+Sources:
+https://www.w3schools.com/jsref/jsref_sort.asp
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/ceil
+https://www.geeksforgeeks.org/javascript-typedarray-subarray-with-examples/
+
+*/
